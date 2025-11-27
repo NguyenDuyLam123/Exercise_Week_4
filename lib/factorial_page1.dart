9df0221c1bb4dad72001529db43db2c4c1e871d7
Nguyen Duy Lam <thanchet1472004@gmail.com>
2025-11-27 17:34:04 +0700

    UpdateTextField factorial UI

--- lib/factorial_page1.dart
+++ lib/factorial_page1.dart
@@ -3,7 +3,9 @@ import 'package:flutter/foundation.dart';
 
 BigInt factorial(int n) {
   BigInt result = BigInt.one;
-  for (int i = 1; i <= n; i++) result *= BigInt.from(i);
+  for (int i = 1; i <= n; i++) {
+    result *= BigInt.from(i);
+  }
   return result;
 }
 
@@ -15,13 +17,33 @@ class FactorialPage extends StatefulWidget {
 class _FactorialPageState extends State<FactorialPage> {
   bool loading = false;
   String result = "";
+  TextEditingController numberCtrl = TextEditingController();
 
   void calc() async {
-    setState(()=>loading=true);
-    BigInt out = await compute(factorial, 30000);
+    if (numberCtrl.text.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("❗Hãy nhập số cần tính")),
+      );
+      return;
+    }
+
+    int? n = int.tryParse(numberCtrl.text);
+    if (n == null || n < 0) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("⚠ Giá trị không hợp lệ")),
+      );
+      return;
+    }
+
+    setState(() => loading = true);
+
+    BigInt out = await compute(factorial, n);
+
     setState(() {
-      loading=false;
-      result = out.toString().substring(0,200)+"...";
+      loading = false;
+      result = out.toString().length > 200
+          ? out.toString().substring(0, 200) + "..."
+          : out.toString();
     });
   }
 
@@ -29,8 +51,28 @@ class _FactorialPageState extends State<FactorialPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Isolate Factorial")),
-      body: Center(
-        child: loading ? CircularProgressIndicator() : Text(result.isEmpty ? "Press to start" : result),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: numberCtrl,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "Nhập số cần tính giai thừa",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            loading
+                ? CircularProgressIndicator()
+                : Text(
+              result.isEmpty ? "Chưa có kết quả" : result,
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: calc,
